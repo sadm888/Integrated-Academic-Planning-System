f94698cae41757255adef112f8949562387689a0
Sadhana Mohanraj <sadhanamohanraj26@gmail.com>
2026-02-20 15:07:10 +0530

    to do list added

--- iaps-backend/extensions.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import os
-
-mongo = None
-
-def init_mongo():
-    global mongo
-    client = MongoClient(os.getenv("MONGO_URI"))
-    mongo = client.iaps
--- iaps-backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Models package"""
-
-from .user import UserModel
-from .classroom import ClassroomModel
-from .semester import SemesterModel
-from .document import DocumentModel
-
-__all__ = ['UserModel', 'ClassroomModel', 'SemesterModel', 'DocumentModel']
